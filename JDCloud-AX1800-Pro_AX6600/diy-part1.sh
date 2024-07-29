1d30cabc7a45335bc443187f1fef82f860d2a2a0
test1jcvbdx <105480444+test1jcvbdx@users.noreply.github.com>
2024-07-29 21:45:26 +0800

    Update diy-part1.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part1.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 git clone https://github.com/kenzok8/openwrt-packages package/kenzo
-git clone https://github.com/kenzok8/small package/small
+git clone https://github.com/kenzok8/small-package package/small
 
 # iStore
 echo >> feeds.conf.default
